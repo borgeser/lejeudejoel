62e234e0b2c4809b0e57c5ef820a981cfc98b03a
Roland Borgese <roland.borgese@fabernovel.com>
2020-06-11 00:46:28 +0200

    Delete useless code

--- game.js
+++ game.js
@@ -7,10 +7,6 @@ const gameOptions = {
     },
     width: 800,
     height: 600,
-    destroySpeed: 200,
-    fallSpeed: 100,
-    slideSpeed: 300,
-    localStorageName: "samegame"
 };
 
 const engineConfig = {
@@ -59,7 +55,6 @@ class MainScene extends Phaser.Scene {
 
     create() {
         this.engine = new GameEngine(engineConfig);
-        this.score = 0;
         this.cellSprites = [];
         this.animalSprites = [];
         this.diceSprite = null;
@@ -68,9 +63,6 @@ class MainScene extends Phaser.Scene {
         this.drawDice();
         this.canPlay = true;
         this.input.on("pointerdown", this.tileSelect, this);
-        this.savedData = localStorage.getItem(gameOptions.localStorageName) == null ? {
-            score: 0
-        } : JSON.parse(localStorage.getItem(gameOptions.localStorageName));
     }
 
     drawField() {
@@ -154,80 +146,6 @@ class MainScene extends Phaser.Scene {
         this.animalSprites[startRow][startCol] = null;
         this.animalSprites[endRow][endCol]?.destroy();
         this.animalSprites[endRow][endCol] = startSprite;
-    }
-
-    makeGemsFall() {
-        let movements = this.sameGame.arrangeBoard();
-        if(movements.length == 0){
-            this.makeGemsSlide();
-        }
-        else{
-            let fallingGems = 0;
-            movements.forEach(function(movement){
-                fallingGems ++;
-                this.tweens.add({
-                    targets: this.sameGame.getCustomDataAt(movement.row, movement.column),
-                    y: this.sameGame.getCustomDataAt(movement.row, movement.column).y + gameOptions.cellSize * movement.deltaRow,
-                    duration: gameOptions.fallSpeed * Math.abs(movement.deltaRow),
-                    callbackScope: this,
-                    onComplete: function(){
-                        fallingGems --;
-                        if(fallingGems == 0){
-                            this.makeGemsSlide();
-                        }
-                    }
-                })
-            }.bind(this));
-        }
-    }
-    makeGemsSlide() {
-        let slideMovements = this.sameGame.compactBoardToLeft();
-        if(slideMovements.length == 0){
-            this.endOfMove();
-        }
-        else{
-            let movingGems = 0;
-            slideMovements.forEach(function(movement){
-                movingGems ++;
-                this.tweens.add({
-                    targets: this.sameGame.getCustomDataAt(movement.row, movement.column),
-                    x: this.sameGame.getCustomDataAt(movement.row, movement.column).x + gameOptions.cellSize * movement.deltaColumn,
-                    duration: Math.abs(gameOptions.slideSpeed * movement.deltaColumn),
-                    ease: "Bounce.easeOut",
-                    callbackScope: this,
-                    onComplete: function(){
-                        movingGems --;
-                        if(movingGems == 0){
-                            this.endOfMove();
-                        }
-                    }
-                });
-            }.bind(this))
-        }
-    }
-    endOfMove() {
-        if(this.sameGame.stillPlayable(2)){
-            this.canPlay = true;
-        }
-        else{
-            let bestScore = Math.max(this.score, this.savedData.score);
-            localStorage.setItem(gameOptions.localStorageName,JSON.stringify({
-                score: bestScore
-          	}));
-            let timedEvent =  this.time.addEvent({
-                delay: 7000,
-                callbackScope: this,
-                callback: function(){
-                    this.scene.start("PlayGame");
-                }
-            });
-            if(this.sameGame.nonEmptyItems() == 0){
-                this.gameText.text = "Congratulations!!";
-            }
-            else{
-                this.gameText.text = "No more moves!!!";
-            }
-        }
     }
 }
 
@@ -397,219 +315,6 @@ class GameEngine {
         this.diceRolled = false;
         this.selectedPawn = null;
     }
-
-    // returns an object with all connected items starting at (row, column)
-    listConnectedItems(row, column) {
-        if(!this.validPick(row, column) || this.gameArray[row][column].isEmpty){
-            return;
-        }
-        this.colorToLookFor = this.gameArray[row][column].value;
-        this.floodFillArray = [];
-        this.floodFillArray.length = 0;
-        this.floodFill(row, column);
-        return this.floodFillArray;
-    }
-
-    // returns the number of connected items starting at (row, column)
-    countConnectedItems(row, column) {
-        return this.listConnectedItems(row, column).length;
-    }
-
-    // removes all connected items starting at (row, column)
-    removeConnectedItems(row, column) {
-        let items = this.listConnectedItems(row, column);
-        items.forEach(function(item){
-            this.gameArray[item.row][item.column].isEmpty = true;
-        }.bind(this))
-    }
-
-    // returs true if in the board there is at least a move with a minimum minCombo items
-    stillPlayable(minCombo) {
-        for(let i = 0; i < this.getRows(); i ++){
-            for(let j = 0; j < this.getColumns(); j ++){
-                if(!this.isEmpty(i, j) && this.countConnectedItems(i, j) >= minCombo){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    // returns the amount of non empty items on the board
-    nonEmptyItems(minCombo) {
-        let result = 0;
-        for(let i = 0; i < this.getRows(); i ++){
-            for(let j = 0; j < this.getColumns(); j ++){
-                if(!this.isEmpty(i, j) ){
-                    result ++;
-                }
-            }
-        }
-        return result;
-    }
-
-    // flood fill routine
-    // http://www.emanueleferonato.com/2008/06/06/flash-flood-fill-implementation/
-    floodFill(row, column) {
-        if(!this.validPick(row, column) || this.isEmpty(row, column)){
-            return;
-        }
-        if(this.gameArray[row][column].value == this.colorToLookFor && !this.alreadyVisited(row, column)){
-            this.floodFillArray.push({
-                row: row,
-                column: column
-            });
-            this.floodFill(row + 1, column);
-            this.floodFill(row - 1, column);
-            this.floodFill(row, column + 1);
-            this.floodFill(row, column - 1);
-        }
-    }
-
-    // arranges the board, making items fall down. Returns an object with movement information
-    arrangeBoard() {
-        let result = []
-
-        // falling down
-        if(this.fallingDown){
-            for(let i = this.getRows() - 2; i >= 0; i --){
-                for(let j = 0; j < this.getColumns(); j ++){
-                    let emptySpaces = this.emptySpacesBelow(i, j);
-                    if(!this.isEmpty(i, j) && emptySpaces > 0){
-                        this.swapItems(i, j, i + emptySpaces, j)
-                        result.push({
-                            row: i + emptySpaces,
-                            column: j,
-                            deltaRow: emptySpaces
-                        });
-                    }
-                }
-            }
-        }
-
-        // falling up
-        else{
-            for(let i = 1; i < this.getRows(); i ++){
-                for(let j = 0; j < this.getColumns(); j ++){
-                    let emptySpaces = this.emptySpacesAbove(i, j);
-                    if(!this.isEmpty(i, j) && emptySpaces > 0){
-                        this.swapItems(i, j, i - emptySpaces, j)
-                        result.push({
-                            row: i - emptySpaces,
-                            column: j,
-                            deltaRow: -emptySpaces
-                        });
-                    }
-                }
-            }
-        }
-        return result;
-    }
-
-    // checks if a column is completely empty
-    isEmptyColumn(column) {
-        return this.emptySpacesBelow(-1, column) == this.getRows();
-    }
-
-    // counts empty columns to the left of column
-    countLeftEmptyColumns(column){
-        let result = 0;
-        for(let i = column - 1; i >= 0; i --){
-            if(this.isEmptyColumn(i)){
-                result ++;
-            }
-        }
-        return result;
-    }
-
-    // compacts the board to the left and returns an object with movement information
-    compactBoardToLeft() {
-        let result = [];
-        for(let i = 1; i < this.getColumns(); i ++){
-            if(!this.isEmptyColumn(i)){
-                let emptySpaces = this.countLeftEmptyColumns(i);
-                if(emptySpaces > 0){
-                    for(let j = 0; j < this.getRows(); j ++){
-                        if(!this.isEmpty(j, i)){
-                            this.swapItems(j, i, j, i - emptySpaces)
-                            result.push({
-                                row: j,
-                                column: i - emptySpaces,
-                                deltaColumn: -emptySpaces
-                            });
-                        }
-                    }
-                }
-            }
-        }
-        return result;
-    }
-
-    // replenishes the board and returns an object with movement information
-    replenishBoard() {
-        let result = [];
-        for(let i = 0; i < this.getColumns(); i ++){
-            if(this.isEmpty(0, i)){
-                let emptySpaces = this.emptySpacesBelow(0, i) + 1;
-                for(let j = 0; j < emptySpaces; j ++){
-                    let randomValue = Math.floor(Math.random() * this.items);
-                    result.push({
-                        row: j,
-                        column: i,
-                        deltaRow: emptySpaces
-                    });
-                    this.gameArray[j][i].value = randomValue;
-                    this.gameArray[j][i].isEmpty = false;
-                }
-            }
-        }
-        return result;
-    }
-
-    // returns the amount of empty spaces below the item at (row, column)
-    emptySpacesBelow(row, column) {
-        let result = 0;
-        if(row != this.getRows()){
-            for(let i = row + 1; i < this.getRows(); i ++){
-                if(this.isEmpty(i, column)){
-                    result ++;
-                }
-            }
-        }
-        return result;
-    }
-
-    // returns the amount of empty spaces above the item at (row, column)
-    emptySpacesAbove(row, column) {
-        let result = 0;
-        if(row != 0){
-            for(let i = row - 1; i >=0; i --){
-                if(this.isEmpty(i, column)){
-                    result ++;
-                }
-            }
-        }
-        return result;
-    }
-
-    // swap the items at (row, column) and (row2, column2)
-    swapItems(row, column, row2, column2) {
-        let tempObject = Object.assign(this.gameArray[row][column]);
-        this.gameArray[row][column] = Object.assign(this.gameArray[row2][column2]);
-        this.gameArray[row2][column2] = Object.assign(tempObject);
-    }
-
-    // returns true if (row, column) is already in floodFillArray array
-    alreadyVisited(row, column) {
-        let found = false;
-        this.floodFillArray.forEach(function(item) {
-            if(item.row == row && item.column == column) {
-                found = true;
-            }
-        });
-        return found;
-    }
-
 }
 
 class Dice {
