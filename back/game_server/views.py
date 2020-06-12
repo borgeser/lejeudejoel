4fcf8243547254f5a4b2d60980dfd380408b4504
Roland Borgese <roland.borgese@fabernovel.com>
2020-06-12 16:26:10 +0200

    Add Django channels example (chat)

--- back/game_server/views.py
+++ back/game_server/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'game_server/index.html')
+
+
+def room(request, room_name):
+    return render(request, 'game_server/room.html', {
+        'room_name': room_name
+    })
--- back/ledosdejoel/settings.py
+++ back/ledosdejoel/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'game_server',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -69,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ledosdejoel.wsgi.application'
+ASGI_APPLICATION = "ledosdejoel.routing.application"
 
 
 # Database
