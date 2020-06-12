571018b11a09c9c33cd5981060607904079a67ef
Roland Borgese <roland.borgese@fabernovel.com>
2020-06-12 17:45:27 +0200

    Implement channels layers

--- back/ledosdejoel/settings.py
+++ back/ledosdejoel/settings.py
@@ -72,6 +72,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ledosdejoel.wsgi.application'
 ASGI_APPLICATION = "ledosdejoel.routing.application"
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
