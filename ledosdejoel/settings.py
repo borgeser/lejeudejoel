fa598dfae3f82a37779d0335d46bf906db3fe561
Roland Borgese <roland.borgese@fabernovel.com>
2021-04-03 10:50:18 +0200

    Update REDIS URL

--- ledosdejoel/settings.py
+++ ledosdejoel/settings.py
@@ -87,7 +87,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL')],
+            "hosts": [os.environ.get('HEROKU_REDIS_AMBER_URL')],
         },
     },
 }
